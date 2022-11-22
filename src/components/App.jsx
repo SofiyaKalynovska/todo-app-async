275fe838555d1677cc2dc2e2798e4bcb7209e9f0
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-11-22 18:17:49 +0200

    Add data fetching to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,17 @@ import { Layout } from 'components/Layout/Layout';
 import { AppBar } from 'components/AppBar/AppBar';
 import { TaskForm } from 'components/TaskForm/TaskForm';
 import { TaskList } from 'components/TaskList/TaskList';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchTasks } from 'redux/operations';
 
 export const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
+  
   return (
     <Layout>
       <AppBar />
